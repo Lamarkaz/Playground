a6ecb9985293942986e33f651cfbc5ef3d824bf4
Muhammed <mselrakabawi@gmail.com>
2018-08-22 12:08:55 +0200

    Added build

---
+++ wallet/dist/precache-manifest.03c3a895f9be111a2e8258a458e6e0dc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "e8d90c1a903db06ae1ee",
+    "url": "/js/chunk-vendors.23a031a7.js"
+  },
+  {
+    "revision": "92d71db347d91b0d5ace",
+    "url": "/js/app.faaf58e2.js"
+  },
+  {
+    "revision": "f7612fce4f406623bbd142baaa73eab3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d6659e3cb59c9cc6a15f957d130777d2",
+    "url": "/img/wallet-icon.d6659e3c.svg"
+  },
+  {
+    "revision": "e8d90c1a903db06ae1ee",
+    "url": "/css/chunk-vendors.2d022094.css"
+  },
+  {
+    "revision": "92d71db347d91b0d5ace",
+    "url": "/css/app.dff67ec8.css"
+  }
+];
