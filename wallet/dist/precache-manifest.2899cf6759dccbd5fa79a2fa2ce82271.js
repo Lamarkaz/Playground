9d8dfe95f24ba31dac1c185b667410f5230e38ec
nourharidy <nourharidy@yahoo.com>
2018-08-24 10:32:57 +0200

    Added new build

---
+++ wallet/dist/precache-manifest.2899cf6759dccbd5fa79a2fa2ce82271.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "e39d89e6b862988ecd23",
+    "url": "/js/chunk-vendors.53f95b20.js"
+  },
+  {
+    "revision": "c8356a7bfb08dc228f7c",
+    "url": "/js/app.8511f685.js"
+  },
+  {
+    "revision": "b5fd3535f63ea562d97d501821dcf685",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d6659e3cb59c9cc6a15f957d130777d2",
+    "url": "/img/wallet-icon.d6659e3c.svg"
+  },
+  {
+    "revision": "e39d89e6b862988ecd23",
+    "url": "/css/chunk-vendors.45e60389.css"
+  },
+  {
+    "revision": "c8356a7bfb08dc228f7c",
+    "url": "/css/app.ec1baf9e.css"
+  }
+];
